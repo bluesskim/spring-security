20e68b9828fca85e7369c4ca3626c4549b4af881
aaron.kss <bluesskim@naver.com>
2021-02-24 10:44:02 +0900

    first commit

---
+++ src/main/java/com/bluesskim/springcoresecurity/SpringcoresecurityApplication.java
@@ -0,0 +1,13 @@
+package com.bluesskim.springcoresecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoresecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoresecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bluesskim/springcoresecurity/SpringcoresecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluesskim.springcoresecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoresecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
